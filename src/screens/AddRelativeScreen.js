1fd6098d7fb715b33c6912db91e18b4a43880833
Gabriel Ikpaetuk <gabrielikpaetuk@byui.edu>
2021-01-31 13:11:01 -0700

    Add forms and input sections to AddRelative

--- src/screens/AddRelativeScreen.js
+++ src/screens/AddRelativeScreen.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Title, Paragraph, TextInput} from 'react-native-paper';
 
 const AddRelativeScreen = () => {
+	const [firstName, setFirstName] = React.useState('');
+	const [lastName, setLastName] = React.useState('');
+
+	console.log(firstName)
+	console.log(lastName)
+
 	return (
-		<View>
-			<Text>Add Relative</Text>
-		</View>
+		<Card style={styles.card}>
+		<Card.Content>
+		  <Title>Add Information</Title>
+		  <TextInput label="First name" value={firstName} onChangeText ={(newText) =>setFirstName(newText)}/>
+
+
+		  <TextInput label="Last name" value={lastName} onChangeText ={(newText) =>setLastName(newText)} underlineColor="green" selectionColor='green'/>
+		</Card.Content>
+	  </Card>
 	);
 };
 
 export default AddRelativeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	card:{
+		margin: 30,
+	}
+});
+
