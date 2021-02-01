012ca7ecd6204cb22d5066354fc82248ae82d2da
Gabriel Ikpaetuk <gabrielikpaetuk@byui.edu>
2021-02-01 02:18:02 -0700

    add date picker

--- src/screens/AddRelativeScreen.js
+++ src/screens/AddRelativeScreen.js
@@ -1,32 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Card, Title, Paragraph, TextInput} from 'react-native-paper';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Card, Title, TextInput } from "react-native-paper";
+import DatePickerComponent from "../components/DatePickerComponent";
 
 const AddRelativeScreen = () => {
-	const [firstName, setFirstName] = React.useState('');
-	const [lastName, setLastName] = React.useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [date, setDate] = useState(new Date());
 
-	console.log(firstName)
-	console.log(lastName)
+  const onDateSelect = (selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+    console.log(currentDate);
+  };
 
-	return (
-		<Card style={styles.card}>
-		<Card.Content>
-		  <Title>Add Information</Title>
-		  <TextInput label="First name" value={firstName} onChangeText ={(newText) =>setFirstName(newText)}/>
+  return (
+    <>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Title>Add Information</Title>
 
+          <View style={styles.inputContainer}>
+            <TextInput
+              label="First name"
+              value={firstName}
+              onChangeText={(newText) => setFirstName(newText)}
+              autoCorrect={false}
+              underlineColor="green"
+            />
+          </View>
 
-		  <TextInput label="Last name" value={lastName} onChangeText ={(newText) =>setLastName(newText)} underlineColor="green" selectionColor='green'/>
-		</Card.Content>
-	  </Card>
-	);
+          <View style={styles.inputContainer}>
+            <TextInput
+              label="Last name"
+              value={lastName}
+              onChangeText={(newText) => setLastName(newText)}
+              autoCorrect={false}
+              underlineColor="green"
+            />
+          </View>
+          <View>
+            <DatePickerComponent onDateSelect={onDateSelect} date={date} />
+          </View>
+        </Card.Content>
+      </Card>
+    </>
+  );
 };
 
 export default AddRelativeScreen;
 
 const styles = StyleSheet.create({
-	card:{
-		margin: 30,
-	}
+  card: {
+    margin: 30,
+  },
+  inputContainer: {
+    marginBottom: 10,
+  },
 });
-
